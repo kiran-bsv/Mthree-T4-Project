2e1b05126c1018ccd7be1e0e7a77a7d0d6879bea
kiran-bsv <boddepallisai@iitbhilai.ac.in>
2025-03-27 22:37:59 +0530

    prometherus , grafana .log setup : Pratik code

--- Backend/server.py
+++ Backend/server.py
@@ -1,7 +1,24 @@
 from app import app, db
 from socket_handler import socketio
+from prometheus_flask_exporter import PrometheusMetrics
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 # import eventlet
 # import eventlet.wsgi
+
+# Set up Prometheus metrics
+metrics = PrometheusMetrics(app)
+
+# Set up logging for Loki
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+log_handler = RotatingFileHandler('logs/app.log', maxBytes=1000000, backupCount=3)
+log_handler.setLevel(logging.INFO)
+log_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
+app.logger.addHandler(log_handler)
+
 
 if __name__ == '__main__':
     with app.app_context():
@@ -9,5 +26,6 @@ if __name__ == '__main__':
     # socketio.run(app, debug=True, port=5000, host="0.0.0.0", use_reloader=False)
     # socketio.init_app(app, async_mode="eventlet")
     # eventlet.wsgi.server(eventlet.listen(("0.0.0.0", 5000)), app)
+    app.logger.info("Starting Flask SocketIO App")
     socketio.run(app, port=5000)
     
--- run.sh
+++ run.sh
@@ -32,6 +32,7 @@ kubectl apply -f be-deployment.yaml
 echo "Creating the deployment - frontend"
 kubectl apply -f fe-deployment.yaml
 
-echo "Porforwarding the services fronend 5173:80 and backend 5000:5000"
-kubectl port-forward svc/frontend-service 5173:80 -n uber
-kubectl port-forward svc/backend-service 5000:5000 -n uber
+
+# echo "Porforwarding the services fronend 5173:80 and backend 5000:5000"
+# kubectl port-forward svc/frontend-service 5173:80 -n uber
+# kubectl port-forward svc/backend-service 5000:5000 -n uber
