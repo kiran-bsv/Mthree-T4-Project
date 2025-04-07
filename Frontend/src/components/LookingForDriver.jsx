3aa6f79daa1800e1d2b03527295fa207e298b121
VaibhavKumarGupta3303 <vaibhav3746@gmail.com>
2025-04-07 16:50:43 +0530

    added comment

--- Frontend/src/components/LookingForDriver.jsx
+++ Frontend/src/components/LookingForDriver.jsx
@@ -1,7 +1,22 @@
+/*
+  LookingForDriver Component
+
+  - This component is shown while the system is searching for an available driver after a ride is requested.
+  - It displays:
+    • A back/close button to exit the panel.
+    • A title indicating the current action (searching for a driver).
+    • A relevant image for visual context.
+    • Key ride details including:
+        - Pickup and destination locations
+        - Estimated distance and duration
+        - Fare based on selected vehicle type
+  - It takes props like pickup, destination, distance, duration, vehicleType, and fare to render this data dynamically.
+*/
 import React from 'react'
 
 const LookingForDriver = (props) => {
     return (
+        // Root container
         <div>
             <h5 className='p-1 text-center w-[93%] absolute top-0' onClick={() => {
                 props.setVehicleFound(false)
@@ -10,6 +25,7 @@ const LookingForDriver = (props) => {
 
             <div className='flex gap-2 justify-between flex-col items-center'>
                 <img className='h-20' src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg" alt="" />
+                {/* Ride details container */}
                 <div className='w-full mt-5'>
                     <div className='flex items-center gap-5 p-3 border-b-2'>
                         <i className="ri-map-pin-user-fill"></i>
@@ -28,7 +44,6 @@ const LookingForDriver = (props) => {
                     <div className='flex items-center gap-5 p-3 border-b-2'>
                         <i className="ri-time-line"></i>
                         <div>
-                            {/* <h3 className='text-lg font-medium'>{props.distance}kms&nbsp;&nbsp;{props.duration?.[ props.vehicleType ]}hrs</h3> */}
                             <h3 className='text-lg font-medium'>
                                 {props.distance}kms&nbsp;&nbsp;
                                 {(() => {
@@ -39,13 +54,14 @@ const LookingForDriver = (props) => {
                                     return `${hours}hr ${minutes}min`;
                                 })()}
                                 </h3>
+                                  {/* Duration text */}
                             <p className='text-sm -mt-1 text-gray-600'>Duration,&nbsp; Distance </p>
                         </div>
                     </div>
                     <div className='flex items-center gap-5 p-3'>
                         <i className="ri-currency-line"></i>
+                          {/* Fare info */}
                         <div>
-                            {/* <h3 className='text-lg font-medium'>{props.duration[props.vehicleType]}hrs</h3> */}
                             <h3 className='text-lg font-medium'>₹{props.fare[ props.vehicleType ]} </h3>
                             <p className='text-sm -mt-1 text-gray-600'>Cash</p>
                         </div>
